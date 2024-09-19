1bdf16f713abc6233a544ba2ed39c7fa03037272
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-09-18 22:16:42 -0400

    Added hot reloading using tsx for development

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import ViteExpress from 'vite-express';
+
+const app: Express = express();
+const port: number = 3000;
+
+const API = '/api/v1';
+
+app.get(`${API}/ping`, (_: Request, res: Response) => {
+    // Sends a friendly message and a 200 status (implicitly)
+    res.send('Hello, this is the Express API.');
+});
+
+app.get(`${API}/test`, (req: Request, res: Response) => {
+    // Sends test JSON data
+    res.json([1, 2, 3, 4, 5]);
+});
+
+ViteExpress.listen(app, port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
