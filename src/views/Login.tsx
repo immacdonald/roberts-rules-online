69a8af5d42ee5c3835781e641b5fc7f2d5e29a0d
Quang <126427971+theantigone@users.noreply.github.com>
2024-09-19 15:15:29 -0400

    Updated artwork and interface

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,22 +1,23 @@
 import {FC} from 'react';
-import style from "./login.module.scss";
+import style from './login.module.scss';
 
 const Login: FC = () => {
     return (
         <main>
-            <h1>Login</h1>
-            <p>This is the login page.</p>
-
             <div className={style.formGroup}>
                 <h2 className={style.title}>Login</h2>
-                <form id="loginForm">
-                    <p>Username: <label htmlFor="username"></label><input type="text" name="username" id="username"
-                                                                          required/></p>
-                    <p>Password: <label htmlFor="password"></label><input type="password" name="password" id="password"
-                                                                          required/></p>
-                    <button type="submit" id={style.loginButton} className="btn">Login</button>
+                <form id='loginForm'>
+                    <div className={style.inputGroup}>
+                        <label htmlFor='email'>Email</label>
+                        <input type='text' name='email' id='email' required/>
+                    </div>
+                    <div className={style.inputGroup}>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' name='password' id='password' required/>
+                    </div>
+                    <button type='submit' id={style.loginButton} className='btn'>Sign In</button>
                 </form>
-                <a href="./NotFound.tsx" id="forgotPassword">Forgot Password?</a>
+                <a href='./NotFound.tsx' id='forgotPassword'>Forgot Password?</a>
             </div>
             {/*<script>
                 const form = document.getElementById('loginForm');
