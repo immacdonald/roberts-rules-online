55a4e9f224c0a29b394d16b3aea3760ab469b43b
Kayla Anderson <kganderson01@email.wm.edu>
2024-09-19 12:49:00 -0400

    added basic homepage and images

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,30 @@
 import { FC } from 'react';
 import style from './Home.module.scss';
+import book from '../images/book.png';
+import lines from '../images/lines.png';
+import miniWebsite from '../images/miniWebsite.png';
 
 const Home: FC = () => {
     return (
-        <main>
-            <h1 className={style.title}>Robert's Rules of Order</h1>
-            <h2>Home Page</h2>
-            <p>This is the home page.</p>
-        </main>
+    <main>
+        <h1 className={style.title}>Robert's Rules Online</h1>
+        <h2>Organized And Efficient
+        Meetings─every time</h2>
+        <p>Robert’s Rules Online gives you the tools to manage, record, and streamline meetings so that no time is wasted </p>
+        <img src={book} alt="robert rule's book" />
+        <h3>Welcome to Robert’s Rules Online</h3>
+        <p>Streamline your meetings and enhance collaboration with Robert Rules Online, the comprehensive digital platform designed to make parliamentary procedure accessible and efficient. Whether you're a seasoned chairperson or new to the world of organized meetings, our user-friendly interface provides the tools you need to navigate Robert's Rules of Order with ease.</p>
+
+        <img src={lines} alt="lines" />
+
+        <h3>Empower Your Team</h3>
+        <p>At Robert’s Rules Online, we believe that effective communication and democratic decision-making are the cornerstones of any successful organization. Our platform offers intuitive features that simplify the process of making motions, conducting votes, and managing discussions. Say goodbye to confusion and hello to clarity as your team engages in meaningful dialogue and reaches consensus with confidence.</p>
+
+        <img src={miniWebsite} alt="mini website" />
+
+        <h4>Pages</h4>
+        <p>links to pages will go here</p>
+    </main>
     );
 };
 
