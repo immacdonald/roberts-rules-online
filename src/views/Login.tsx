6804a1a67f525276b55ec4aaa788aaae27e92092
Quang <126427971+theantigone@users.noreply.github.com>
2024-09-19 12:57:38 -0400

    Fixed merge conflicts between LoginPage and main branches

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,12 +1,35 @@
-import { FC } from 'react';
+import {FC} from 'react';
+import style from "./login.module.scss";
 
 const Login: FC = () => {
     return (
         <main>
             <h1>Login</h1>
             <p>This is the login page.</p>
+
+            <div className={style.formGroup}>
+                <h2 className={style.title}>Login</h2>
+                <form id="loginForm">
+                    <p>Username: <label htmlFor="username"></label><input type="text" name="username" id="username"
+                                                                          required/></p>
+                    <p>Password: <label htmlFor="password"></label><input type="password" name="password" id="password"
+                                                                          required/></p>
+                    <button type="submit" id={style.loginButton} className="btn">Login</button>
+                </form>
+                <a href="src/404.html" id="forgotPassword">Forgot Password?</a>
+            </div>
+            {/*<script>
+                const form = document.getElementById('loginForm');
+                form.addEventListener('submit', function (event) {
+                event.preventDefault();
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                console.log('Username:', username);
+                console.log('Password:', password);
+            });
+            </script>*/}
         </main>
     );
 };
 
-export { Login };
+export {Login};
