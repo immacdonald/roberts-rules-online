e1ded9bc7ed90bac4a5b93678fc2cbe7021d3d35
Peter Greek <xerxes468893@gmail.com>
2024-09-23 23:08:40 -0400

    Created Socket Interface

---
+++ src/interfaces/socket.tsx
@@ -0,0 +1,21 @@
+import {FC, useState, useEffect, useContext} from 'react';
+import { io, Socket } from 'socket.io-client';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000'; // "undefined" means the URL will be computed from the `window.location` object
+// Singleton
+export class MySocket {
+	public socket:Socket;
+	public static instance: MySocket = new MySocket();
+
+	private constructor() {
+		console.log("Creating socket...");
+		this.socket = io(URL);
+	}
+
+	public doSomething(){
+		//...
+	}
+
+	public doOtherThings(){
+		//...
+	}
+}
