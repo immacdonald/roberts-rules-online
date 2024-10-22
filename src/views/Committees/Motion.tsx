53376e461da5e73f11bf024df602917399204ceb
Quang <126427971+theantigone@users.noreply.github.com>
2024-10-22 01:48:20 -0400

    Added buttons

--- src/views/Committees/Motion.tsx
+++ src/views/Committees/Motion.tsx
@@ -1,11 +1,19 @@
 import { FC } from 'react';
 import { Page } from '../../components';
+import styles from './Motion.module.scss';
 
 const Motion: FC = () => {
     return (
         <Page>
-            <h1>Motion Page</h1>
-            <p>This is the Motion page.</p>
+            <h1>Motion about what day to bird watch</h1>
+            <p>I move that Saturday become the day the group gathers to watch birds</p>
+            <div className={styles.buttonContainer}>
+                <button className={styles.upvote}>Reply 👍</button>
+                <button className={styles.downvote}>Reply 👎</button>
+                <button className={styles.noVote}>Reply 🤷‍♀️</button>
+                <button className={styles.postpone}>Postpone 📅</button>
+                <button className={styles.amend}>Amend 🖋️</button>
+            </div>
         </Page>
     );
 };
