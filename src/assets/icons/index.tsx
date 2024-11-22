0dbe8daee439e8632d7d898f44b37b130d7b1f3c
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-11-22 00:22:36 -0500

    User name changes and redesigned profile page

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,6 +1,7 @@
 import { FC, SVGProps } from 'react';
 import BackArrowIconSVG from './back_arrow.svg?react';
 import ChairIconSVG from './chair.svg?react';
+import CloseIconSVG from './close.svg?react';
 import DeleteIconSVG from './delete.svg?react';
 import EditIconSVG from './edit.svg?react';
 import HomeIconSVG from './home.svg?react';
@@ -14,6 +15,7 @@ const asIcon = (WrappedSVG: FC<SVGProps<SVGSVGElement>>): FC<SVGProps<SVGSVGElem
 export const RobertRulesOnlineIcon = asIcon(RobertsRulesOnlineIconSVG);
 export const BackArrowIcon = asIcon(BackArrowIconSVG);
 export const ChairIcon = asIcon(ChairIconSVG);
+export const CloseIcon = asIcon(CloseIconSVG);
 export const DeleteIcon = asIcon(DeleteIconSVG);
 export const EditIcon = asIcon(EditIconSVG);
 export const HomeIcon = asIcon(HomeIconSVG);
